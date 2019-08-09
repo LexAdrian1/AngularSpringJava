1127b39b7a74e7f49bf9250b1d503f6d1e73686b
LexAdrian <lexadrian1@gmail.com>
2019-08-09 16:46:35 -0500

    Modulos

--- Aplicacion/src/app/app.module.ts
+++ Aplicacion/src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { ListarComponent } from './Persona/listar/listar.component';
 import { AgregarComponent } from './Persona/agregar/agregar.component';
 import { EditarComponent } from './Persona/editar/editar.component';
+import { FormsModule } from '@angular/forms';
+import { ServiceService } from '../app/Service/service.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ListarComponent,
     AgregarComponent,
-    EditarComponent
+    EditarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
